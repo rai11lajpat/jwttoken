dbe19736a4e3318bb6a13b8b7fdf18aec584f8f6
Lajpat Rai <lajpatsankhla@gmail.com>
2024-10-01 15:19:48 +0500

    two user password added with User, userdetailservise, Password Encoder

---
+++ src/main/java/com/example/jwttoken/config/ApConfig.java
@@ -0,0 +1,30 @@
+package com.example.jwttoken.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ApConfig {
+    
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1= User.builder().username("lajpat rai").password(passwordEncoder().encode("hello")).roles("ADMIN").build();
+        UserDetails user2= User.builder().username("jai").password(passwordEncoder().encode("kumar")).roles("ADMIN").build();
+
+
+        return new InMemoryUserDetailsManager(user1,user2);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+}
